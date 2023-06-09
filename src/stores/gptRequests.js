c9bc77ce96b41dbc606d6fbf578ce6803b1731d4
obrien-avatech <obrien.mcquade@avatechnologyllc.com>
2023-06-09 14:16:35 -0700

    sgdg

--- src/stores/gptRequests.js
+++ src/stores/gptRequests.js
@@ -20,16 +20,33 @@ export const useGptRequestsStore = defineStore({
 		}
 	},
 	actions: {
-		async startMarketingPlan (payload) {
+		// async startMarketingPlan (payload) {
 
+		// 	try {
+		// 		const response = await axios.post('/.netlify/functions/start-marketing-plan-background',
+		// 			payload)
+
+		// 		this.taskStatus = 'pending'
+
+		// 		if (response.status === 200) {
+		// 			this.taskStatus = 'complete'
+		// 		} else {
+		// 			console.error('Error:', response.data.error)
+		// 			this.taskStatus = 'error'
+		// 		}
+		// 	} catch (error) {
+		// 		console.error(error)
+		// 	}
+		// }
+		async startMarketingPlan (payload) {
 			try {
-				const response = await axios.post('/.netlify/functions/start-marketing-plan-background',
-					payload)
+				const response = await axios.post('/.netlify/functions/start-marketing-plan-background', payload)
 
 				this.taskStatus = 'pending'
 
-				if (response.status === 200) {
-					this.taskStatus = 'complete'
+				if (response.status === 202) {
+					const taskStatusEndpoint = response.headers['location']
+					await this.pollForResponse(taskStatusEndpoint)
 				} else {
 					console.error('Error:', response.data.error)
 					this.taskStatus = 'error'
@@ -37,6 +54,41 @@ export const useGptRequestsStore = defineStore({
 			} catch (error) {
 				console.error(error)
 			}
+		},
+
+		async pollForResponse (endpoint) {
+			const maxAttempts = 10
+			const pollInterval = 5000 // 3 seconds
+			let attempts = 0
+
+			while (attempts < maxAttempts) {
+				try {
+					const response = await axios.get(endpoint)
+					const data = response.data
+
+					if (data.taskStatus === 'complete') {
+						this.taskStatus = 'complete'
+						const marketingPlan = data.marketingPlan
+						console.log(marketingPlan)
+						return // Exit the loop and function
+					} else if (data.taskStatus === 'error') {
+						console.error('Error:', data.error)
+						this.taskStatus = 'error'
+						return // Exit the loop and function
+					}
+
+					attempts++
+					await new Promise(resolve => setTimeout(resolve, pollInterval))
+				} catch (error) {
+					console.error(error)
+					this.taskStatus = 'error'
+					return // Exit the loop and function
+				}
+			}
+
+			console.error('Response not available within the specified number of attempts.')
+			this.taskStatus = 'error'
 		}
+
 	}
 })
