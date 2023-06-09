4e4f6a47cfda98a18e513c0a18b7c4bb81ec4ed2
obrien-avatech <obrien.mcquade@avatechnologyllc.com>
2023-06-09 09:38:48 -0700

    api change

--- netlify/functions/start-marketing-plan-background.js
+++ netlify/functions/start-marketing-plan-background.js
@@ -18,35 +18,37 @@ exports.handler = async (event, context) => {
 	const User = Parse.Object.extend('User')
 	const query = new Parse.Query(User)
 
-	const user = await query.get(userId) // Get the user object
-	user.set('taskStatus', 'pending') // Set taskStatus to 'pending'
-	await user.save()
-		.then(() => {
-			console.log('User saved successfully.')
+	try {
+		const user = await query.get(userId) // Get the user object
+		user.set('taskStatus', 'pending') // Set taskStatus to 'pending'
+		await user.save() // Wait for the save operation to complete
+		console.log('User saved successfully.')
+
+		const configuration = new Configuration({
+			organization: 'org-5C2c3cHJsvmv3cCc5WWNhZs1',
+			apiKey: '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',
 		})
-		.catch((error) => {
-			console.error('Error saving user: ', error)
+
+		const openai = new OpenAIApi(configuration)
+		const response = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: payload }],
 		})
 
-	const configuration = new Configuration({
-		organization: 'org-5C2c3cHJsvmv3cCc5WWNhZs1',
-		apiKey: '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',
-	})
+		// Once the task is complete, update the taskStatus and taskResult
+		user.set('taskStatus', 'complete')
+		user.set('taskResult', response.data.choices)
+		await user.save() // Wait for the save operation to complete
 
-	const openai = new OpenAIApi(configuration)
-	const response = await openai.createChatCompletion({
-		model: 'gpt-3.5-turbo',
-		messages: [{ role: 'user', content: payload }],
-	})
-
-	// Once the task is complete, update the taskStatus and taskResult
-	user.set('taskStatus', 'complete')
-	user.set('taskResult', response.data.choices)
-	await user.save()
-
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ taskId: userId, marketingPlan: response.data.choices[0].message.content })
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ taskId: userId, marketingPlan: response.data.choices[0].message.content })
+		}
+	} catch (error) {
+		console.error('Error: ', error)
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ error: error.toString() })
+		}
 	}
 }
-
