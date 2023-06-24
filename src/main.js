8de20bc194f2fc665bb6a51fa1c6519bff74280d
obrien-avatech <obrien.mcquade@avatechnologyllc.com>
2023-06-23 23:42:38 -0700

    new font

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import '../public/fonts.css'
 
 import BaseModal from '@/components/ui/BaseModal.vue'
 import DropdownSelect from '@/components/ui/DropdownSelect.vue'
